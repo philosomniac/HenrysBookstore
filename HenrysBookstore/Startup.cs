af8939cc2033f2f6b304325b8d9e5d3cf208af9e
Philosomniac <shamiton42@yahoo.com>
2016-12-12 19:25:25 -0600

    Initial Commit

---
+++ HenrysBookstore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HenrysBookstore.Startup))]
+namespace HenrysBookstore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
